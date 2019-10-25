a5d36b0a611f869c913f0e334378c13b7a444c57
raju <rajupushpad311@gmail.com>
2019-10-26 05:13:20 +0530

    updation in jsonwebtoken

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,30 +7,23 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_request
-    if request.headers['Authorization'].present?
-    	 result = JsonWebToken.decode(request.headers['Authorization'])
-    	 if result.present?
-    	 	user_id = result[0]['user_id']
-    	 	current_user = User.find(user_id)
-    	 	if current_user.present?
+     result = JsonWebToken.decode(request.headers['Authorization'])
+     if result.present?
+      user_id = result[0]['user_id']
+      current_user = User.find(user_id)
+      if current_user.present?
 
-    	 	else
-    	 		render json: {
-            status: 400,
-            message: 'User not present'
-            }
-    	 	end
-    	 else
-    	 	render json: {
-            status: 400,
-            message: 'InValid Auth Token'
-            }
-    	 end
-  else
-    render json: {
-      status: 400,
-      message: 'no token provided'
-      }
+      else
+        render json: {
+          status: 400,
+          message: 'User not present'
+          }
+      end
+     else
+      render json: {
+          status: 400,
+          message: 'InValid Auth Token'
+          }
+     end
   end
-end
 end
