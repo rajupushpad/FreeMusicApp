99818962502a962fed55e3d0a3c55700b7071b35
raju <rajupushpad311@gmail.com>
2019-10-26 03:31:35 +0530

    precompile issue

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module FreeMusicApp
     config.load_defaults 5.2
     config.eager_load = true
     config.eager_load_paths << Rails.root.join('lib')
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
