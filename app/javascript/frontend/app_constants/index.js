42b362e945df1b8f138a41207077fba4e0837681
raju <rajupushpad311@gmail.com>
2019-10-26 04:07:15 +0530

    url updation

--- app/javascript/frontend/app_constants/index.js
+++ app/javascript/frontend/app_constants/index.js
@@ -1,5 +1,5 @@
 export const  APP_CONSTANTS = {
-	URL: 'https://test-app-share-video.herokuapp.com/',
+	URL: 'https://test-app-share-video.herokuapp.com',
 	CURRENT_USER: 'CURRENT_USER',
 	GET_USER: 'GET_USER',
 	POST_ADD_SUCCESS: 'POST_ADD_SUCCESS'
